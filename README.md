f276a905f09bf2d86baee7d0198940f5e2c6d94b
Marcio T <mlt4356-github@yahoo.com>
2022-01-30 12:15:33 -0700

    Added license

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # ESP32 Tutorials
 
 Small ESP32 Tutorials and Sample Sketches
+
+* esp32cam-timelapse-microsd : Record a timelapse to a MicroSD card using the ESP32-CAM
+* simple-time-with-location : Syncing time with the proper timezone and DST for a location. 
--- simple-time-with-location/time_zones.h
+++ simple-time-with-location/time_zones.h
@@ -1,3 +1,22 @@
+/****************************************************************************
+ *   ESP32 Hacks (c) 2021 Marcio Teixeira                                   *
+ *                                                                          *
+ *   https://github.com/marciot/esp32-hacks                                 *
+ *                                                                          *
+ *   This program is free software: you can redistribute it and/or modify   *
+ *   it under the terms of the GNU General Public License as published by   *
+ *   the Free Software Foundation, either version 3 of the License, or      *
+ *   (at your option) any later version.                                    *
+ *                                                                          *
+ *   This program is distributed in the hope that it will be useful,        *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of         *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          *
+ *   GNU General Public License for more details.                           *
+ *                                                                          *
+ *   To view a copy of the GNU General Public License, go to the following  *
+ *   location: <http://www.gnu.org/licenses/>.                              *
+ ****************************************************************************/
+
 // Data imported from https://github.com/nayarsystems/posix_tz_db/blob/master/zones.csv
 const char *timezones[] = {
     "Africa/Abidjan","GMT0",
